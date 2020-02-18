e4a694fc0615c71a779c2b2b22e4d5b416771d18
André DUPIN <a.dupin@idsoft.lan>
2020-02-18 15:55:07 +0100

    Updates

---
+++ precache-manifest.d94b93367a26edc08e09808c8ca65ee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c2ed23845f6e69814f222840bbe710",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b153d59be5804a54494a",
+    "url": "/my-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b153d59be5804a54494a",
+    "url": "/my-app/static/js/2.593b8f15.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/my-app/static/js/2.593b8f15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "441b5f01a15fc2f6535e",
+    "url": "/my-app/static/js/main.62697f0f.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  }
+]);
